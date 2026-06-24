fa71b5836a1a9f75c0c8e77ea0b8855983b59bfd
Lakshay Chhabra <lakshayyy15@gmail.com>
2026-06-25 03:29:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (35.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
 int t[46];
     int solve(int n){
         if(n<0) return 0;
@@ -11,8 +12,21 @@ int t[46];
         int two = solve(n-2);
         return t[n]=one+two;
     }
+    */
     int climbStairs(int n) {
+        /*
         memset(t,-1,sizeof(t));
         return solve(n);
+        */
+        //bottomup
+        vector<int>t(n+1,0);
+        if(n==1 || n==2 || n==3) return n;
+        t[1]=1;
+        t[2]=2;
+        t[0]=0;
+        for(int i=3;i<=n;i++){
+            t[i]=t[i-1]+t[i-2];
+        }
+        return t[n];
     }
 };
